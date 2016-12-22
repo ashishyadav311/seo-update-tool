f5251ebe95f89d41ad964cdbe1dd5296f079d1f7
Ashish Yadav <ashish.yadav1@proptiger.com>
2016-12-22 15:42:05 +0530

    query select

--- src/routes/routeHandlers/pingback.js
+++ src/routes/routeHandlers/pingback.js
@@ -6,26 +6,25 @@ var config = require('services/config');
 module.exports.routeHandler = function(req, res, next) {
 
     query.getAllTemplateIds().then(function(response) {
-            let url_category_id = {};
-            for (var i in response) {
-                for (var j in response[i]) {
-                    url_category_id[response[i]["id"]] = response[i]["name"];
-                }
+        let url_category_id = {};
+        for (var i in response) {
+            for (var j in response[i]) {
+                url_category_id[response[i]["id"]] = response[i]["name"];
             }
-            res.send(config);
-            config = url_category_id;
-            console.log(config)
-        }, function(error) {
-            res.send(error);
-            console.log('---###---')
-        })
-        // query.getAllMapping(parseInt(req.query.id)).then(function(response) {
-        //     res.send(response);
-        //     console.log('--------')
-        // }, function(error) {
-        //     res.send(error);
-        //     console.log('---###---')
-
+        }
+        res.send(config);
+        config = url_category_id;
+        console.log(config)
+    }, function(error) {
+        res.send(error);
+        console.log('---###---')
+    })
+    // query.getAllMapping(parseInt(req.query.id)).then(function(response) {
+    //     res.send(response);
+    //     console.log('--------')
+    // }, function(error) {
+    //     res.send(error);
+    //     console.log('---###---')
     // })
 
 };

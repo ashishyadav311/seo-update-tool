46c4e5544e9d0674030dec389e8e4e07668140ff
Ashish Yadav <ashish.yadav1@proptiger.com>
2016-12-22 15:41:53 +0530

    query select

--- src/services/query.js
+++ src/services/query.js
@@ -2,7 +2,7 @@ var db = require('./db');
 
 function getAllTemplateIds() {
     var query = 'Select id ,name  FROM url_categories;'
-    return new Promise(function(reject, resolve) {
+    return new Promise(function(resolve, reject) {
         db.executeQuery(query, '', function(error, result) {
             if (error) {
                 reject(error)
@@ -13,6 +13,20 @@ function getAllTemplateIds() {
     });
 }
 
+function getAllMapping(id) {
+    var query = 'select * from url_category_breadcrum_mapping where url_category_id = ?;'
+    return new Promise(function(resolve, reject) {
+        db.executeQuery(query, [id], function(error, result) {
+            if (error) {
+                reject(error)
+            } else {
+                resolve(result);
+            }
+        });
+    });
+}
+
 module.exports = {
-    getAllTemplateIds: getAllTemplateIds
+    getAllTemplateIds: getAllTemplateIds,
+    getAllMapping: getAllMapping
 }
